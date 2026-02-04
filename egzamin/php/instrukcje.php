0c09d90830b5573c1bd2ce54560b8fc2d5acd5ac
IgLel <jcold8419@gmail.com>
2026-02-04 13:35:44 +0100

    gotowa instrukcja

---
+++ egzamin/php/instrukcje.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instrukcje</title>
+</head>
+<body>
+
+<?php
+
+$a=5;
+$b=6;
+$c="napis"; 
+
+ if($a==5)
+    {
+        echo "a jest rowne 5";
+    }
+ else if($a<5)
+    {
+        echo "a jest mniejsze od 5";
+    }
+ else 
+    {
+        echo "a jest wieksze od 5";
+    }     
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
