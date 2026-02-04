608364c38639e50563c3d3a934e65d6e5436bd13
IgLel <jcold8419@gmail.com>
2026-02-04 14:03:07 +0100

    instrukcje

--- egzamin/php/instrukcje.php
+++ egzamin/php/instrukcje.php
@@ -7,37 +7,60 @@
 </head>
 <body>
 
-<?php
+ <?php
 
-$a=5;
-$b=6;
-$c="napis"; 
+ $a=5;
+ $b=6;
+ $c="napis"; 
 
- if($a==5)
+    if($a==5)
     {
         echo "a jest rowne 5";
     }
- else if($a<5)
+    echo "<br>";
+
+    if($a<5)
     {
         echo "a jest mniejsze od 5";
     }
- else 
+    else 
     {
         echo "a jest wieksze od 5";
-    }     
+    }
+    echo "<br>";
 
+    if($b>4)
+    {
+        echo "b jest wieksze od 4 ";
+    }
+    echo "<br>";
 
+    if($c=="napis")
+    {
+        echo $c;
+    }
+    else 
+    {
+        echo "zmienna c to nie napis";
+    }
+    echo "<br>";
+    
+    $a=1;
+    $b=-6;
+    $c=-8;
+    $delta=$b*$b-4*$a*$c;
 
+    if($delta>0)
+    {
+        echo "delta ma dwa rozwiazania";
+    }
 
-
-
-
-
-
-
-
-
-?>
+    else if($delta==0)
+    {
+        echo "delta ma jedno rozwiązanie";
+    }    
+  
+ ?>
 
 
 
