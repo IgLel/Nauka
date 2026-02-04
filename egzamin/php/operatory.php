52a686dadf107f0da169c5240378ee93aec6772b
IgLel <jcold8419@gmail.com>
2026-02-04 11:06:23 +0100

    gotowe operatory

---
+++ egzamin/php/operatory.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operatory</title>
+</head>
+<body>
+   <?php
+   echo "<h1>Operatory</h1>";
+   $a=5;
+   $b="napis";
+   $c=7;
+   $d="7";
+   echo "a= ".$a. "b= ".$b. "c= ".$c. "d= ".$d. "<br>";
+   echo "a= ".$a."<br>";
+   echo "b= ".$b."<br>";
+   echo "c= ".$c."<br>";
+   echo "d= ".$d."<br>";
+   $wynik;
+   
+   $wynik=($a<$c);
+   echo "porownanie a < c to: ".$wynik."<br>";
+   $wynik=($a<=$c);
+   echo "porownanie a <= c to: ".$wynik."<br>";
+   $wynik=($a>$c);
+    echo "porownanie a > c to: ".$wynik."<br>";
+    $wynik=($a>=$c);
+    echo "porownanie a >= c to: ".$wynik."<br>";
+    $wynik=($a==$c);
+    echo "porownanie a == c to: ".$wynik."<br>";
+    $wynik=($a!=$c);
+    echo "porownanie a != c to: ".$wynik."<br>";
+    $wynik=($a===$c);
+    echo "porownanie a === c to: ".$wynik."<br>";
+
+   ?>
+
+
+
+
+
+
+</body>
+</html>
