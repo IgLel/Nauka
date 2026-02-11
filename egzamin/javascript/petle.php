c2a52061cf3abaa1b4ea1aedd7c6208712e18e46
IgLel <jcold8419@gmail.com>
2026-02-11 13:32:57 +0100

    zmiana

--- egzamin/javascript/petle.php
+++ egzamin/javascript/petle.php
@@ -1,11 +1,35 @@
 <?php
 
-   for($i=0;$i<10, $i++)
+   for($i=0;$i<10;$i++)
     {
         echo "i= " .$i."<br>";
     }
+   
+    echo"<p>petla WHILE </p>";
+    $j=11;
+    while($j<10)
+    {
+        echo "j= ".$j. "<br>";
+        $j++;
+    }
+    echo"<p>petla DO WHILE </p>";
+    $k=11;
+    do
+    {
+        echo "k= " .$k. "<br>";
+        $k++;
+    }
+    while($k<10);
 
-
+    $m=1;
+    $suma=0;
+    while($m<=100)
+    {
+       $suma+=$m;
+       $m++;
+    }    
+    echo "suma= ".$suma;
+   $k=1;
 
 
 
