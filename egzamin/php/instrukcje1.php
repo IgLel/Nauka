5facf5f358722115053585a84eb1ddcfdd8fa20f
IgLel <jcold8419@gmail.com>
2026-02-18 08:18:45 +0100

    ok

--- egzamin/php/instrukcje1.php
+++ egzamin/php/instrukcje1.php
@@ -8,12 +8,30 @@
 <body>
 
  <?php
-  $a=6;
-  if(a%0)
-   {
-    echo "a jest parzysta"
-   } 
 
+  $liczba=4;
+
+ if ($liczba % 2 == 0)
+   {
+    echo "$liczba jest liczbą parzystą.";
+   }
+  else
+   {
+    echo "$liczba jest liczbą nieparzystą.";
+   }
+ 
+    echo "<br>";
+
+    echo "zadanie 2";
+    echo "<br>";
+    $a=4;
+    $b=3;
+     
+
+    if($a % $b == 0)
+    {
+        echo " jest podzielna przez drugą. ";
+    }
 
 
 
