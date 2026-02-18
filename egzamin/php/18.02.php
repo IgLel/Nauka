a1240fa335d86866c16c299c2938e0a081f37650
IgLel <jcold8419@gmail.com>
2026-02-18 10:53:24 +0100

    p

---
+++ egzamin/php/18.02.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+ $liczba=4
+
+ if($liczba % 2 == 0)
+ {
+    echo "$liczba jest liczba parzysta" ;
+ }
+  
+ else
+ {
+  echo "$liczba jest liczba nieparzysta";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
+ 
+
+
+
+
+
+
+</body>
+</html>
