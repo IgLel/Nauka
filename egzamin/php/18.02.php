a697e2fd5bda7b4f4759c8004b196049c87cec83
IgLel <jcold8419@gmail.com>
2026-02-18 14:39:29 +0100

    11

--- egzamin/php/18.02.php
+++ egzamin/php/18.02.php
@@ -7,7 +7,13 @@
 </head>
 <body>
 <?php
- $liczba=4
+ $liczba=4;
+ $a=5;
+ $b=6;
+ $wiek=20;
+
+ echo "Zadanie 1:";
+ echo "<br>";
 
  if($liczba % 2 == 0)
  {
@@ -19,11 +25,103 @@
   echo "$liczba jest liczba nieparzysta";
  }
 
+  echo "<br>";
+  echo "<br>";
+  echo "Zadanie 2:";
+  echo "<br>";
+ 
+  if($a % $b == 0)
+ {
+  echo " liczba $a jest podzielna przez liczba $b i wynosi: " +wynik;
+ }
+ 
+ else
+ {
+   echo "liczba $a nie jest podzielna przez $b";
+ }
 
+ echo "<br>";
+ echo "<br>";
+ echo "Zadanie 3:";
+ echo "<br>";
 
+ if($liczba >= 1 && $liczba <=10)
+ {
+    echo "Liczba $liczba jest w przedziale <1,10>";
+ }
+ 
+ else if($liczba >= 17 && $liczba <=21)
+ {
+    echo "Liczba $liczba jest w przedziale <17,21>";
+ }
 
+ echo "<br>";
+ echo "<br>";
+ echo "Zadanie 4:";
+ echo "<br>";
+ 
+  if($liczba > 0)
+ {
+   echo "Liczba jest wieksza od 0";
+ }
+ 
+ else if($liczba < 0)
+ {
+   echo "liczba jest mniejsza od 0";
+ }
 
+ else if($liczba == 0)
+ {
+   echo "liczba jest rowna 0";
+ }
 
+ echo "<br>";
+ echo "<br>";
+ echo "Zadanie 5:";
+ echo "<br>";
+ 
+ if($wiek < 11)
+ {
+   echo "Dziecko";
+ }
+ 
+ else if($wiek >= 11 && $wiek <=17)
+ {
+    echo "Nastolatek";
+ }
+
+ else if($wiek >= 18)
+ {
+   echo "Dorosly";
+ }
+
+ echo "<br>";
+ echo "<br>";
+ echo "Zadanie 6:";
+ echo "<br>";
+ 
+ $haslo="haslo";
+ if (strlen($haslo) < 5)
+  {
+    echo "bardzo slabe"
+  }
+   
+   if else (strlen($haslo) >= 5 && strlen($haslo) < 8)
+  {
+    echo "slabe"
+  }
+
+  if else(strlen($haslo) >= 9 && strlen($haslo) < 11)
+  {
+    echo "silne"
+  }
+
+  
+  if else(strlen($haslo) > 11)
+  {
+    echo " bardzo silne"
+  }
+ 
 
 
 
