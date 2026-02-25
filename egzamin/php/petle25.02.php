e01f5d31acefabb98ef676790f88b400d6112a3c
IgLel <jcold8419@gmail.com>
2026-02-25 11:05:51 +0100

    petl

---
+++ egzamin/php/petle25.02.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+  echo "<table border="1">";
+
+  for($i= 1; $i <= 2; $i++)
+    {
+        echo "<br>";
+        echo "<tr>";
+    }
+
+    for($j = 1; $j <= 10; $j++)
+        {
+            echo "<td>Wiersz $i, Kolumna $j</td>";
+        }
+
+    echo "</tr>";
+    echo "<br>"
+
+    echo "</table>"
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ?>
+</body>
+</html>
