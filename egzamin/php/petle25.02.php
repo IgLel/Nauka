f620b06638359cea06f754ea2ff66ad04e05de32
IgLel <jcold8419@gmail.com>
2026-03-02 12:35:23 +0100

    02.03

--- egzamin/php/petle25.02.php
+++ egzamin/php/petle25.02.php
@@ -7,23 +7,42 @@
 </head>
 <body>
   <?php
-  echo "<table border="1">";
+  echo "Zadanie 1";
+  echo "<br>";
+  echo "<table border='1'>";
 
   for($i= 1; $i <= 2; $i++)
     {
-        echo "<br>";
+        
         echo "<tr>";
-    }
+    
 
     for($j = 1; $j <= 10; $j++)
         {
             echo "<td>Wiersz $i, Kolumna $j</td>";
+            
         }
 
-    echo "</tr>";
-    echo "<br>"
+        echo "</tr>";
+    
+    }
+    echo "</table>";
 
-    echo "</table>"
+  echo "<br>";
+  echo "Zadanie 2";
+  echo "<br>";
+
+
+ for ($i = 1; $i <= 2; $i++) 
+
+ {
+    for ($j = 1; $j <= 10; $j++) 
+     {
+        echo "+";
+     }
+
+    echo "<br>";
+ }
   
 
 
